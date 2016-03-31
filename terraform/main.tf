465cf344e59e0c72770b5542d5fcd4009c45a711
John Rowe <john.rowe@aquilent.com>
2016-03-31 10:34:08 -0400

    Added RDS Support

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,6 +102,44 @@ resource "aws_elb" "rancher_elb" {
     }
 }
 
+resource "aws_db_subnet_group" "rancher" {
+    name = "rancher"
+    description = "Datagov Rancher DB Subnet"
+    subnet_ids = [""${lookup(var.subnet_id, "us-east-1_privateA")}"", ""${lookup(var.subnet_id, "us-east-1_privateB")}""]
+    tags {
+        Name = "Rancher DB subnet group"
+    }
+}
+
+resource "aws_db_security_group" "rancher" {
+    name = "datagov_rancher_db"
+    description = "Rancher RDS SG"
+
+    ingress {
+        security_group_id = "${aws_security_group.rancher.id}"
+    }
+}
+
+resource "aws_rds_instance" "rancher_db" {
+	engine = "mysql"
+	engine_version = "5.6.27"
+	allocated_storate = ${var.rds_disk_size}
+	storage_type = "gp2"
+	instance_class = "${var.rds_size}"
+	identifier = "rancher"
+	final_snapshot_identifier = "datagov_rancher"
+	copy_tags_to_snapshot = true
+	name = "cattle"
+	username = "${var.rds_user}"
+	password = "${var.rds_pass}"
+	publicly_accessible = false
+	db_subnet_group_name = "${aws_db_subnet_group.rancher.id}"
+	vpc_security_group_ids = "${aws_db_subnet_group.rancher.id}"
+	tags {
+		Name = "Rancher Database"
+	}
+}
+
 resource "aws_instance" "rancher_main" {
     ami = "${lookup(var.rancher_amis, var.region)}"
     instance_type = "m3.xlarge"
